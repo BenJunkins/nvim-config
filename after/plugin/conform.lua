458f4132018ea546a1ee6fb345175de80fbaf96f
BenJunkins <benjamin.junkins@gmail.com>
2026-01-07 18:56:33 -0500

    Add python formatting isort and black

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -2,22 +2,23 @@
 local conform = require("conform")
 
 conform.setup({
-    formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        lua = { "stylua" }
-    },
+	formatters_by_ft = {
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+		json = { "prettier" },
+		yaml = { "prettier" },
+		markdown = { "prettier" },
+		lua = { "stylua" },
+		python = { "isort", "black" },
+	},
 
-    format_on_save = {
-        lsp_fallback = true, -- If Prettier isn't available, try the LSP (safety net)
-        async = false,       -- false = blocks the save until format is done (safer)
-        timeout_ms = 500,    -- Give up if formatting takes longer than 0.5 seconds
-    },
+	format_on_save = {
+		lsp_fallback = true, -- If Prettier isn't available, try the LSP (safety net)
+		async = false, -- false = blocks the save until format is done (safer)
+		timeout_ms = 500, -- Give up if formatting takes longer than 0.5 seconds
+	},
 })
