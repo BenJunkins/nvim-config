8c102b2be4d3f68a71d02114083666e9c7179e17
BenJunkins <benjamin.junkins@gmail.com>
2026-04-08 17:26:32 -0400

    move telescope back to telescope.lua

--- lua/McKearnyPlum/remap.lua
+++ lua/McKearnyPlum/remap.lua
@@ -1,15 +1,3 @@
--------------------------- Setup telescope -----------------------------------
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Ts find files" })
-vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Ts find git files" })
-
--- Grep
-vim.keymap.set("n", "<leader>gl", builtin.live_grep, { desc = "Live Grep" })
-vim.keymap.set("n", "<leader>gs", function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end, { desc = "Grep string" })
--------------------------- End telescope -------------------------------------
-
 -- High level quality of life
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Explore files" })
