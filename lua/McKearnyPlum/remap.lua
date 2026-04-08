b5cb28bd82fe23be6372efb65da8c5ed218f6a87
BenJunkins <benjamin.junkins@gmail.com>
2026-04-08 14:26:02 -0400

    add desc to remaps and move telescope maps to remap.lua

--- lua/McKearnyPlum/remap.lua
+++ lua/McKearnyPlum/remap.lua
@@ -1,36 +1,53 @@
+-------------------------- Setup telescope -----------------------------------
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Ts find files" })
+vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Ts find git files" })
+
+-- Grep
+vim.keymap.set("n", "<leader>gl", builtin.live_grep, { desc = "Live Grep" })
+vim.keymap.set("n", "<leader>gs", function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end, { desc = "Grep string" })
+-------------------------- End telescope -------------------------------------
+
+-- High level quality of life
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Explore files" })
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Escape out of insert" })
+vim.keymap.set("n", "Q", "<nop>", { desc = "No operation of Ex mode" })
 
--- Moves blocks of code selected in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- Moving blocks of code
+vim.keymap.set("x", "J", ":m '>+1<CR>gv=gv", { desc = "Move highlighted down" })
+vim.keymap.set("x", "K", ":m '<-2<CR>gv=gv", { desc = "Move highlighted up" })
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+-- Cursor management
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Line join without moving cursor" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Center cursor in next search" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Center cursor in prev search" })
 
-vim.keymap.set("x", "<leader>p", '"_dP')
+-- Putting
+vim.keymap.set("v", "<leader>p", '"_dP', { desc = "Put without overwriting reg" })
+vim.keymap.set("n", "<leader>p", '"0p', { desc = "Put last yank" })
 
-vim.keymap.set("n", "<leader>y", '"+y')
-vim.keymap.set("x", "<leader>y", '"+y')
-vim.keymap.set("n", "<leader>Y", '"+Y')
+-- Yanking
+vim.keymap.set("n", "<leader>y", '"+y', { desc = "Yank motion into clipboard" })
+vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "Yank line into clipboard" })
+vim.keymap.set("x", "<leader>y", '"+y', { desc = "Yank highlight into clipboard" })
 
-vim.keymap.set("n", "<leader>d", '"_d')
-vim.keymap.set("v", "<leader>d", '"_d')
+-- Deleting
+vim.keymap.set("n", "<leader>d", '"_d', { desc = "Delete avoid register" })
+vim.keymap.set("v", "<leader>d", '"_d', { desc = "Delete avoid register" })
 
-vim.keymap.set("i", "jk", "<Esc>")
+-- Quickfix window
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz", { desc = "Next in Quickfix" })
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz", { desc = "Prev in Quickfix" })
 
-vim.keymap.set("n", "Q", "<nop>")
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-
--- Opens and closes terminal
+-- Terminal
 vim.keymap.set("n", "<leader>t", function()
-	vim.cmd.vsplit()
-	vim.cmd.term()
+	vim.cmd.vsplit() -- Opens new window
+	vim.cmd.term() -- Opens terminal
 	vim.cmd.startinsert() -- Immediately start typing
-end)
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+end, { desc = "Opens terminal in new window" })
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
