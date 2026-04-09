edb27ceb5b011849865a5905da87eb860018badd
BenJunkins <benjamin.junkins@gmail.com>
2026-04-08 23:02:17 -0400

    add undo directory path

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,3 +1,13 @@
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
+-- Define the path strictly
+local undodir = vim.fn.stdpath("data") .. "/undo"
+
+-- Force Neovim to create the directory if it fails to do so automatically
+if vim.fn.isdirectory(undodir) == 0 then
+	vim.fn.mkdir(undodir, "p")
+end
+
+-- Set the directory and turn on the feature
+vim.opt.undodir = undodir
 vim.opt.undofile = true
